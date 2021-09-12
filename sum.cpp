19f4e9e83f66e5debc4a8e1f698700fa1e4744ea
Aditya Thakur <adityathakur672003@gmail.com>
2021-09-13 00:03:38 +0530

    git log

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout<<"Enter First Number- \n";
+    cin>>a;
+
+    cout<<"Enter Second Number- \n";
+    cin>>b;
+
+    cout<< "Sum is " <<a+b;
+}
